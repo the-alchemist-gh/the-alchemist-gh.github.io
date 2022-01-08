8ce6f6b1ea7e08d9c69f0c1173e3a1942cf7feb0
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-01-08 03:52:55 +0000

    Added sound and transformation to menu

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 let userName = document.getElementById('visitor-name');
-let getUserName = prompt('Wozop, Kindly enter your name');
+// let getUserName = prompt('Wozop, Kindly enter your name');
+buttonClicked = document.querySelectorAll(".btn");
 
-userName.innerHTML = getUserName;
+
+// userName.innerHTML = getUserName;
+buttonClicked.forEach(btnClick => btnClick.addEventListener('click', ()=>{
+    // console.log(e)
+    const audio = document.querySelector('audio')
+    if(!audio) return; // stop function from running
+    audio.currentTime = 0; //rewind to start
+    audio.play();
+}));
+// for (let i=0; i<buttonClicked.length; i++){
+//   buttonClicked[i].addEventListener('click',function(e){
+//     console.log(e)
+//     const audio = document.querySelector('audio')
+//     if(!audio) return; // stop function from running
+//     audio.currentTime = 0; //rewind to start
+//     audio.play();
+//     // console.log(audio);
+    
+  
+//   });
+// }
